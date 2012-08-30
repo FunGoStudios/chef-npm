502c4925230f965be1f8e971134515de91014f7d
Nicola Brisotto <nicola@breezit.net>
2012-08-30 12:30:30 +0200

    Check version and skip already installed

--- providers/package.rb
+++ providers/package.rb
@@ -1,8 +1,14 @@
 action :install do
-  cmd  = "npm -g install #{new_resource.name}"
-  cmd += "@#{new_resource.version}" if new_resource.version
-  execute "install NPM package #{new_resource.name}" do
-    command cmd
+  status = package_status(new_resource.name, true)
+  name,version = name_and_version_from_string(new_resource.name)
+  if status[:installed] && (version.nil? || version == status[:version] )
+      Chef::Log.info("#{status[:name]} #{status[:version]} already installed: skip install")
+  else
+    cmd  = "npm -g install #{new_resource.name}"
+    cmd += "@#{new_resource.version}" if new_resource.version
+    execute "install NPM package #{new_resource.name}" do
+      command cmd
+    end
   end
 end
 
@@ -32,4 +38,21 @@ action :uninstall_local do
     cwd path
     command cmd
   end
+end
+
+private
+
+def package_status(package, is_global)
+  info = `npm #{is_global ? "-g" : ""} ls -p  -l cube`.chop
+  ret_hash = {}
+  ret_hash[:installed] = info.empty? ? false : true
+  return ret_hash if !ret_hash[:installed]
+  name_and_version = name_and_version_from_string( info.split('/').last.split(':').last )
+  ret_hash[:name] = name_and_version[0]
+  ret_hash[:version] = name_and_version[1]
+  ret_hash
+end
+
+def name_and_version_from_string(package_string)
+  return package_string.split('@')
 end
